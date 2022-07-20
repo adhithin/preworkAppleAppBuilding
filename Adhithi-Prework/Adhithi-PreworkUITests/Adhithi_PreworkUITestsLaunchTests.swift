5796a212c717840dd10a3962deb3eeda48af8752
adhithin <71796291+adhithin@users.noreply.github.com>
2022-07-20 11:43:29 -0700

    Add files via upload

---
+++ Adhithi-Prework/Adhithi-PreworkUITests/Adhithi_PreworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Adhithi_PreworkUITestsLaunchTests.swift
+//  Adhithi-PreworkUITests
+//
+//  Created by adhithi narayana murthy on 7/19/22.
+//
+
+import XCTest
+
+class Adhithi_PreworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
